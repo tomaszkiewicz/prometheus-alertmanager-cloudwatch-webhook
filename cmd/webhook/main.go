351263340f7371eccc6c12a2855d2254d5aaab31
luktom <luktom@luktom.net>
2019-05-20 22:14:24 +0200

    Fix unit

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -68,7 +68,7 @@ func putMetric() error {
 		MetricData: []*cloudwatch.MetricDatum{
 			{
 				MetricName: aws.String(viper.GetString("metric-name")),
-				Unit:       aws.String("Count"),
+				Unit:       aws.String("None"),
 				Value:      aws.Float64(1),
 				Dimensions: []*cloudwatch.Dimension{
 					/*
