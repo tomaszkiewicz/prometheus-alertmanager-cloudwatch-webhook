f8bbb2ed457c5257e35329b65868c14fa4a443f6
Evyatar Cohen <coevyatar@gmail.com>
2019-09-12 14:09:02 +0300

    Update env vars & file structure

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/cloudwatch"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
-	"log"
-	"net/http"
-	"strings"
 )
+
+var sess *session.Session
+var svc *cloudwatch.CloudWatch
 
 func webhook(c *gin.Context) {
 	if err := putMetric(); err != nil {
@@ -20,32 +24,6 @@ func webhook(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, struct{}{})
-}
-
-var sess *session.Session
-var svc *cloudwatch.CloudWatch
-
-func main() {
-	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
-	viper.AutomaticEnv()
-
-	viper.SetDefault("http-port", 8077)
-	viper.SetDefault("metric-name", "DeadMansSwitch")
-	viper.SetDefault("region", "eu-west-1")
-
-	var err error
-
-	sess, err = session.NewSession(&aws.Config{Region: aws.String(viper.GetString("region"))})
-	if err != nil {
-		log.Fatal("unable to create AWS session due to an error:", err)
-	}
-
-	r := setupRouter()
-	listenAddress := fmt.Sprintf(":%d", viper.GetInt("http-port"))
-	log.Printf("listening on: %s", listenAddress)
-	if err := r.Run(listenAddress); err != nil {
-		panic(err)
-	}
 }
 
 func setupRouter() *gin.Engine {
@@ -64,23 +42,40 @@ func putMetric() error {
 	svc = cloudwatch.New(sess)
 
 	_, err := svc.PutMetricData(&cloudwatch.PutMetricDataInput{
-		Namespace: aws.String("Prometheus"),
+		Namespace: aws.String(viper.GetString("NAMESPACE")),
 		MetricData: []*cloudwatch.MetricDatum{
 			{
-				MetricName: aws.String(viper.GetString("metric-name")),
+				MetricName: aws.String(viper.GetString("METRIC_NAME")),
 				Unit:       aws.String("None"),
 				Value:      aws.Float64(1),
-				Dimensions: []*cloudwatch.Dimension{
-					/*
-						{
-							Name:  aws.String("InstanceName"),
-							Value: aws.String("prometheus-k8s-0"),
-						},
-					*/
-				},
+				Dimensions: []*cloudwatch.Dimension{},
 			},
 		},
 	})
 
 	return err
+}
+
+func main() {
+	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+	viper.AutomaticEnv()
+
+	viper.SetDefault("HTTP_PORT", 8077)
+	viper.SetDefault("METRIC_NAME", "DeadMansSwitch")
+	viper.SetDefault("REGION", "eu-west-1")
+	viper.SetDefault("NAMESPACE", "Prometheus")
+
+	var err error
+
+	sess, err = session.NewSession(&aws.Config{Region: aws.String(viper.GetString("REGION"))})
+	if err != nil {
+		log.Fatal("unable to create AWS session due to an error:", err)
+	}
+
+	r := setupRouter()
+	listenAddress := fmt.Sprintf(":%d", viper.GetInt("HTTP_PORT"))
+	log.Printf("listening on: %s", listenAddress)
+	if err := r.Run(listenAddress); err != nil {
+		panic(err)
+	}
 }
